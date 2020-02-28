fd7377146908769217455a4a3fe51af1f7f6d11e
Makiras <xieyunlonglong@gmail.com>
2020-02-28 13:23:41 +0800

    [Fix] Pjax Enlighter scrpit mistake

--- smarty_hankin/assets/js/main.js
+++ smarty_hankin/assets/js/main.js
@@ -40,7 +40,7 @@ function hljsLoad() {
 	if (typeof mermaid == "undefined"){
 		var mermaid={};
 	}else{mermaid.initialize({ "theme": "dark", "logLevel": 5, "arrowMarkerAbsolute": false, "startOnLoad": true, "flowchart": { "htmlLabels": true, "curve": "linear" }, "sequence": { "diagramMarginX": 50, "diagramMarginY": 10, "actorMargin": 50, "width": 150, "height": 65, "boxMargin": 10, "boxTextMargin": 5, "noteMargin": 10, "messageMargin": 35, "mirrorActors": true, "bottomMarginAdj": 1, "useMaxWidth": true }, "gantt": { "titleTopMargin": 25, "barHeight": 20, "barGap": 4, "topPadding": 50, "leftPadding": 75, "gridLineStartPadding": 35, "fontSize": 11, "fontFamily": "\"Open-Sans\", \"sans-serif\"", "numberSectionStyles": 4, "axisFormat": "%Y-%m-%d" }, "class": {}, "git": {} }, '.mermaid');	}
-	EnlighterJS.Util.Helper(document.getElementsByClassName("EnlighterJSRAW"),EnlighterJS_Config);
+	EnlighterJS.Util.Init(EnlighterJS_Config.selector.block, EnlighterJS_Config.selector.inline, EnlighterJS_Config);
 	//弹出垂直菜单
 	$(".menu").click(function () {
 		if ($(this).hasClass("cura")) {
